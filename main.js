b2ed12aca48255dfb561a7123b279b6877b59dcb
Abhi <103061615+abhicodelink@users.noreply.github.com>
2022-08-03 20:59:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,33 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 function modalLoaded(){
   console.log("modalLoaded")
 }
+function check(){
+  img  = document.getElementById("captured_img");
+  classifier.classify(img , gotResult)
+}
+function gotResult(error , result){
+  if(error){
+    console.log(error)
+  }
+  else {
+    console.log(result)
+     if(result[0].label == "amazing"){
+      document.getElementById("result_label").innerHTML = "&#128076;";
+      say = "This is amazing sign and it's meaning is this is looking amazing";
+     }
+     if(result[0].label == "victory" ){
+      document.getElementById("result_label").innerHTML = "&#9996;";
+      say = "This is victory  sign and it's meaning is that was a marvelous victory";
+     }
+     if(result[0].label == "best"){
+      document.getElementById("result_label").innerHTML = "&#128077;";
+      say = "This is best sign and it's meaning is all the best";
+     }
+     speak();
+  }
+}
+function speak(){
+  var synth = window.speechSynthesis;
+  utter_this = new SpeechSynthesisUtterance(say);
+  synth.speak(utter_this)
+}
